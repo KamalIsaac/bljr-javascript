3a00eac1bc60c096590fdaddafcd6fa1b50f0f1b
Muhammad Isaac Kamal <43644002+KamalIsaac@users.noreply.github.com>
2025-07-24 20:26:06 +0700

    first commit

---
+++ Belajar JS Dasar/main.js
@@ -0,0 +1,7 @@
+let saldoAwal = 50000;
+let saldoTambah = 100000;
+const saldoAkhir = saldoAwal + saldoTambah;
+
+alert(
+  `saldo anda Rp ${saldoAwal}, anda menabung sebanyakRp${saldoTambah}. jadi total saldo anda adalah Rp${saldoAkhir}`
+);
